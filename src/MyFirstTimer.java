715f91c841b78b3d89d4932f1656ddb379e104b0
Son <s_nguyen91@u.pacific.edu>
2025-02-19 16:24:04 -0800

    The three things a timer need is a start function to start it, a delay so the timer actually goes in play, and a stop so the timer can end.

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,11 +1,19 @@
-import acm.graphics.*;
-import acm.program.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyFirstTimer extends GraphicsProgram {
+import javax.swing.Timer;
+
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
+
+public class MyFirstTimer extends GraphicsProgram implements ActionListener {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
+	
+	private int count;
+	private Timer t;
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -15,6 +23,21 @@ public class MyFirstTimer extends GraphicsProgram {
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		count = 0;
+		
+		t = new Timer(1000, this);
+		t.start();
+	
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		myLabel.move(5, 0);
+		count++;
+
+		myLabel.setLabel("times called? " + count);
+		if (count == 10) {
+			t.stop();
+		}
 	}
 	
 	public static void main(String[] args) {
